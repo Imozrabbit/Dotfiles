a7e38be29274c7d7efcb74d368229acb8b2ae144
Zrabbit <zrabbit000@gmail.com>
2026-02-16 17:05:54 +0100

    Remove formatters and linters from lsp setup
    Change the lsp.log level to ERROR only to make it less noisy

--- ArchLinux/NeoVim/.config/nvim/lua/general/init.lua
+++ ArchLinux/NeoVim/.config/nvim/lua/general/init.lua
@@ -2,3 +2,7 @@ require("general.keymaps")
 require("general.options")
 require("general.api")
 require("general.lsp")
+
+-- Make the lsp log less noisy
+-- default value : 3 for warnings and errors
+vim.lsp.set_log_level(4)
