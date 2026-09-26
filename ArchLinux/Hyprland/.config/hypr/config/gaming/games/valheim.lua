48cbe5e166a787233410581fe6d57cc062c30c3f
Zrabbit <zrabbit000@gmail.com>
2026-09-26 03:29:38 +0200

    Added a window rule for native valheim build

--- ArchLinux/Hyprland/.config/hypr/config/gaming/games/valheim.lua
+++ ArchLinux/Hyprland/.config/hypr/config/gaming/games/valheim.lua
@@ -1,8 +1,22 @@
--- Valheim, Project Zomboid
 hl.window_rule({
-	name = "Force steam games to be opaque and to appear on workspace 7",
+	name = "Proton Valheim window rule",
 	match = {
 		class = "steam_app_892970",
+		title = "Valheim",
+	},
+	content = "game",
+	confine_pointer = true,
+	workspace = GAME_WORKSPACE,
+	opaque = true,
+	no_blur = true,
+	xray = false,
+	no_shadow = true,
+})
+
+hl.window_rule({
+	name = "Native Valheim window rule",
+	match = {
+		class = "valheim.x86_64",
 		title = "Valheim",
 	},
 	content = "game",
